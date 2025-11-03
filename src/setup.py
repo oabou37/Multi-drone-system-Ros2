a63f8fc2502ca9bcfe4bc3b26b7f34555b5e0dfb
oabou37 <oussamaabouelfaraj37@gmail.com>
2025-11-03 08:26:59 +0000

    Add files via upload

---
+++ src/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+from glob import glob
+
+package_name = 'olive'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, glob('launch/*.launch.py'))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='step',
+    maintainer_email='binon.stepane@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    entry_points={
+        'console_scripts': [
+            'analyse_CAM = olive.analyse_CAM:main',
+            'MAV_manager = olive.MAV_manager:main',
+        ],
+    },
+)
