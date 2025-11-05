e84a989bfe29a02db7b9a0a5ed8ce5109782bee5
oabou37 <oussamaabouelfaraj37@gmail.com>
2025-11-05 18:31:53 +0000

    Update setup.py

--- src/popeye/setup.py
+++ src/popeye/setup.py
@@ -1,32 +1,42 @@
-from setuptools import find_packages, setup
+"""Setup script for the popeye package."""
+
+from setuptools import setup, find_packages
+import os
 from glob import glob
 
 package_name = 'popeye'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='1.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name, glob('launch/*.launch.py')),
-        # ('lib/python3.10/site-packages/' + package_name, ['popeye/utils/FSM_utils.py']),
-        # ('lib/python3.10/site-packages/' + package_name, ['popeye/utils/MAV_utils.py']),
+        # Include launch files
+        (os.path.join('share', package_name, 'launch'), 
+            glob('launch/*.launch.py')),
+        # Include config files
+        (os.path.join('share', package_name, 'config'), 
+            glob('config/*.yaml')),
+        # Include video files (if any)
+        (os.path.join('share', package_name, 'videos'), 
+            glob('popeye/videos/*.mp4')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='step',
-    maintainer_email='binon.stepane@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer='Your Name',
+    maintainer_email='your.email@example.com',
+    description='POPEYE drone control system with ArUco marker detection',
+    license='Apache-2.0',
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'MAV_manager__node = popeye.MAV_manager__node:main',
-            'FSM__node         = popeye.FSM__node:main',
-            'CAM__node         = popeye.CAM__node:main',
-            'ARUCO__node       = popeye.ARUCO__node:main',
-            'ASSERV__node      = popeye.ASSERV__node:main',
+            'MAV_manager_node = popeye.MAV_manager__node:main',
+            'CAM_node = popeye.CAM__node:main',
+            'ARUCO_node = popeye.ARUCO__node:main',
+            'FSM_node = popeye.FSM__node:main',
         ],
     },
 )
